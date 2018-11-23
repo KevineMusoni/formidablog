3ec80868c161abc12f822ebdc51f177984922124
Kevine Musoni <kevinenicky@gmail.com>
2018-11-23 11:04:09 +0300

    initial commit

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,10 +3,4 @@ from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
 
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Add New Pitch',validators = [Required()])
-    submit = SubmitField('Submit')
-class CommentForm(FlaskForm):
-    text = TextAreaField('Leave a comment:',validators=[Required()])
-    submit = SubmitField('Submit')
  
