9c84bf0c1c81c69c74609906591d6d30a5e4267a
Kevine Musoni <kevinenicky@gmail.com>
2018-11-26 05:45:42 +0300

    email subscription

--- app/main/forms.py
+++ app/main/forms.py
@@ -11,9 +11,10 @@ class UpdateBio(FlaskForm):
     bio = StringField("Bio")
     submit = SubmitField("Update")
 
+# blog subscription form
 class EmailForm(FlaskForm):
     email = StringField("Enter email", validators = [Required()])
-    submit = SubmitField("ubscribe")
+    submit = SubmitField("subscribe")
 
 class AddComment(FlaskForm):
     comment = TextAreaField("Comment", validators = [Required()])
