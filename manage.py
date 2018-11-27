10201ad3275c1c1a0831945378f0cb03edca6ebc
Kevine Musoni <kevinenicky@gmail.com>
2018-11-27 09:58:45 +0300

    Heroku redeployment

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from app.models import User, Role, Comment
 from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('test')
+app = create_app('production')
 manager = Manager(app)
 manager.add_command('server',Server)
 migrate = Migrate(app, db)
